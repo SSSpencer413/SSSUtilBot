79f316f14bdd46acb060b10fc72cd235842678dd
Spencer <30614666+SSSpencer413@users.noreply.github.com>
2024-11-23 23:04:51 -0500

    made ping command useful

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 // sample command
 
-const { SlashCommandBuilder, InteractionContextType, ApplicationIntegrationType } = require('discord.js');
+const { SlashCommandBuilder, InteractionContextType, ApplicationIntegrationType, EmbedBuilder } = require('discord.js');
 
 
 module.exports = {
@@ -10,6 +10,20 @@ module.exports = {
 		.setIntegrationTypes("UserInstall")
 		.setContexts([InteractionContextType.Guild, InteractionContextType.BotDM, InteractionContextType.PrivateChannel]),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Pong!', ephemeral: true });
+		let embed = new EmbedBuilder();
+		embed.setDescription("Pong!\n");
+		embed.setColor("Aqua");
+
+		let fields = [
+			{ name: 'App Permissions', value: "- `" + interaction.appPermissions.toArray().join('`\n- `') + "`", inline: true}
+		];
+
+		if (interaction.memberPermissions) {
+			fields.push({ name: 'Your Permissions', value: "- `" + interaction.memberPermissions.toArray().join('`, `') + "`", inline: true});
+		}
+
+		embed.addFields(fields)
+
+		await interaction.reply({ embeds: [embed], ephemeral: true });
 	},
 };
